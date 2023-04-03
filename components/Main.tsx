2dce3b4ba9195a19e2e5ebee7fb3dde512ff584e
Diego Alejandro Poveda <alejopov@gmail.com>
2023-04-02 19:15:52 -0500

    create main layout with sections to be later replaced with their respective components

---
+++ components/Main.tsx
@@ -0,0 +1,15 @@
+const Main = () => (
+  <>
+    {/* Hacer un componente y usarlo en cada una de estas secciones: */}
+    <section /> {/* Semana de la Mujer */}
+    <section /> {/* Formas de Pago */}
+    <section /> {/* Ofertas */}
+    <section /> {/* Suscripción */}
+    <section /> {/* Beneficios */}
+    <section /> {/* Tiendas */}
+    <section /> {/* Descubre */}
+    <section /> {/* Colecciones: Supermercado */}
+    <section /> {/* Categorías */}
+  </>
+);
+export { Main };
