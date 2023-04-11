0088c53b95af421dc57dbd0c2e6bb3764e2b6660
pipesanta <juansantag95@gmail.com>
2023-04-11 17:53:00 -0500

    responsive fixed for discover section

--- sections/Discover.tsx
+++ sections/Discover.tsx
@@ -22,46 +22,49 @@ const ITEMS = [
 ];
 
 export function DiscoverComponent() {
-  return <div id="discover-section" className="w-full">
+  return <section className='mt-[50px] max-w-[1200px] xs:w-[300px] sm:w-[500px] xl:w-full'>
+    <div id="discover-section" className="w-full">
 
-    <SectionTitle
-      title='Descubre'
-      subtitle=''
-      className='mt-[70px]'
-    />
+      <SectionTitle
+        title='Descubre'
+        subtitle=''
+        className='mt-[70px]'
+      />
 
-    <div className="flex flex-col lg:flex-row lg:space-x-2 space-y-2 lg:space-y-0 mt-4 w-full">
+      <div className="flex flex-col lg:flex-row lg:space-x-2 space-y-2 lg:space-y-0 mt-4 w-full">
 
-      {ITEMS.map((item) => {
-        return <div className="flex justify-between rounded-lg w-full bg-white">
+        {ITEMS.map((item) => {
+          return <div className="flex justify-between rounded-lg w-full bg-white">
 
-          <div className="left flex flex-col justify-center px-4 py-4">
-            <span className="text-xs">
-              {item.title}
-            </span>
+            <div className="left flex flex-col justify-center px-4 py-4">
+              <span className="text-xs">
+                {item.title}
+              </span>
 
-            {item.body.map((bodyValue) => <>
-              <span className="text-2xl font-semibold"> {bodyValue} </span>
-            </>)
-            }
+              {item.body.map((bodyValue) => <>
+                <span className="text-2xl font-semibold"> {bodyValue} </span>
+              </>)
+              }
 
-            <div className="flex px-12 py-4 justify-center cursor-pointer text-white">
-              <div className="text-center px-8 py-2 ok-btn-blue text-lg">
-                {item.textBtn}
+              <div className="flex px-12 py-4 justify-center cursor-pointer text-white">
+                <div className="text-center px-8 py-2 ok-btn-blue text-lg">
+                  {item.textBtn}
+                </div>
               </div>
+
+            </div>
+
+            <div className="text-center rounded-xl">
+              <img className="rounded-lg" src={item.pic} alt="" />
             </div>
 
           </div>
+        })
+        }
 
-          <div className="text-center rounded-xl">
-            <img className="rounded-lg" src={item.pic} alt="" />
-          </div>
-
-        </div>
-      })
-      }
+      </div>
 
     </div>
 
-  </div>
+  </section>
 }
